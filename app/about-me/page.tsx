3778f18aedc091cdf939f1b4ad04a57fcbfedcb9
BigDahn <harkinborholarholuwah@gmail.com>
2026-05-02 03:06:58 +0100

    update pages and add Experience component

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,37 +1,29 @@
-"use client"
-
+import Image from "next/image";
 import Contributions from "../components/pages/about-me/Contributions";
 import Hero from "../components/Hero";
+import ArrowIcon from "@/assets/svgs/arrow.svg";
 import { MoveDownIcon } from "lucide-react";
 import SectionContainer from "../components/SectionContainer";
 import ProfileSection from "../components/pages/about-me/Owner";
-import Experiences from "../components/pages/about-me/Experiences";
-import TrustedPlatforms from "../components/pages/about-me/TrustedPlatforms";
-import FooterArc from "../components/FooterArc";
+import Experience from "../components/pages/about-me/Experience";
 
 const Page = () => {
   return (
     <SectionContainer id="about" className="min-h-screen bg-black">
       <div className="px-10">
-        <Hero className="h-screen inset-0">
-          <div className="min-h-[700px] space-y-20 flex flex-col relative justify-center items-center border-gray-400">
+        <Hero className="h-screen">
+          <div className="min-h-[700px] space-y-20 flex flex-col relative justify-center items-center border-b border-gray-400">
             <div className="flex flex-col max-w-[1002px] gap-4 justify-center items-center px-4 py-3  text-center">
               <h1 className="unbounded text-white font-semibold text-[70px] capitalize max-w-[850px] leading-[93%] tracking-[-8%] text-center">
                 A Crypto Trader with 6 years of experience
               </h1>
               <p className="jost text-[30px] text-gray-400 font-[400px] leading-[130%] tracking-[-4%]">
-                I am a crypto trader with deep knowledge about the market. I have
-                partnered with several crypto brands and helped traders achieve the
-                best results in the crypto space
+                I am a crypto trader with deep knowledge about the market. I
+                have partnered with several crypto brands and helped traders
+                achieve the best results in the crypto space
               </p>
             </div>
-            <div onClick={() => {
-              document
-                .getElementById("contributions")
-                ?.scrollIntoView({
-                  behavior: "smooth",
-                })
-            }} className="from-white cursor-pointer flex flex-col justify-center gap-5 py-[34px] px-[11.5px] bg-clip-text to-blue-900 h-64 flex items-center flex-col w-32 rounded-[50%/50%] border border-gray-400 absolute bottom-[0%]">
+            <div className="from-white flex flex-col justify-center gap-5 py-[34px] px-[11.5px] bg-clip-text to-blue-900 h-64 flex items-center flex-col w-32 rounded-[50%/50%] border border-gray-400 absolute bottom-[0%]">
               <MoveDownIcon size={25} />
               <h6 className="jost font-[400px] text-[20px] leading-[130%] text-white/20 tracking-[-6%] text-center">
                 Scroll Down
@@ -41,9 +33,7 @@ const Page = () => {
         </Hero>
         <Contributions />
         <ProfileSection />
-        <Experiences />
-        <TrustedPlatforms />
-        <FooterArc />
+        <Experience />
       </div>
     </SectionContainer>
   );

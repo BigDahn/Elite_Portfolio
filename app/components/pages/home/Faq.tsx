afba1a10872bf63c68d933bd1775e1b1169faa4a
Sh(a)muel <oreoluwaakinbo.oa@gmail.com>
2026-05-04 10:17:19 +0100

    update about me page

--- app/components/pages/home/Faq.tsx
+++ app/components/pages/home/Faq.tsx
@@ -38,6 +38,7 @@ const FAQ = ({ items }: FAQProps) => {
                 key={i}
                 value={`item-${i}`}
                 className="hover:bg-slate-100/5 duration-500 px-5 rounded-lg py-2 h-48.75 flex flex-col justify-center"
+
               >
                 <AccordionTrigger className="jost text-white font-medium text-[15px] hover:no-underline hover:text-white/80 transition-colors [&>svg]:hidden flex justify-between items-start  gap-4 group">
                   <span className="unbounded text-[12px] md:text-[20px] leading-[130%] tracking-[-2%] text-left">

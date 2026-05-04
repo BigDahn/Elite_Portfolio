11955840a01aa1d36e90803089065a1ee880acaf
Sh(a)muel <oreoluwaakinbo.oa@gmail.com>
2026-05-04 14:51:03 +0100

    fix the track record card for responsiveness on tablet

--- app/components/ExternalLinkButton.tsx
+++ app/components/ExternalLinkButton.tsx
@@ -11,9 +11,9 @@ interface ExternalBtnLink {
 
 const ExternalLinkButton = ({ href, text, className, textClassName }: ExternalBtnLink) => {
   return (
-    <button className={clsx("max-w-97.5 h-14 flex items-center justify-center bg-white gap-5 rounded-[63.18px]", className)}>
+    <button className={clsx("max-w-97.5 h-12 md:h-14 flex items-center justify-center bg-white gap-5 rounded-[63.18px]", className)}>
       <Link href={href} className="flex items-center justify-center gap-5">
-        <p className={clsx("jost font-semibold text-[20px] leading-[130%] tracking-[-2%]", textClassName)}>
+        <p className={clsx("jost font-semibold text-[14px] md:text-[20px] leading-[130%] tracking-[-2%]", textClassName)}>
           {text}
         </p>
         <SquareArrowOutUpRight size={20} />

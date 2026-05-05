8ca89eaf961006bd6ee01f967b010f4bc9be02f7
Sh(a)muel <oreoluwaakinbo.oa@gmail.com>
2026-05-05 19:06:05 +0100

    update footer arc

--- app/components/FooterArc.tsx
+++ app/components/FooterArc.tsx
@@ -1,12 +1,8 @@
 "use client";
-
 import Ellipse from "@/assets/svgs/Ellipse 2990.svg";
 import ExternalLinkButton from "./ExternalLinkButton";
-import { ArrowUp } from "lucide-react";
-import { motion } from "motion/react";
 import Image from "next/image";
-import clsx from "clsx"
-
+import clsx from "clsx";
 
 interface FooterArcProps {
   className?: string;
@@ -14,40 +10,38 @@ interface FooterArcProps {
 
 const FooterArc = ({ className }: FooterArcProps) => {
   return (
-    <div className={clsx("h-[500px]  w-screen lg:min-h-175 bottom-0 bg-white relative overflow-hidden left-0 right-0 pt-20  mx-auto z-40", className)}>
-      <div className="bg-black rounded-full h-[5000px] w-[5000px] absolute bottom-0" />
-      <div
-        className="
-      absolute
-      left-1/2
-      top-1/2
-      -translate-x-1/2
-      -translate-y-1/2
-      z-50
-      space-y-5
-    "
-      >
-        <Image
-          className="mx-auto"
-          src="/Frame 2121453894.png"
-          width={50}
-          height={50}
-          alt=""
-        />
-        <p className="text-transparent unbounded bg-clip-text bg-linear-to-b from-white to-blue-800 text-[15px] lg:text-[50px] text-center capitalize font-extrabold leading-[100%] tracking-[-0.06em]">
-          Lets start your <br /> trading journey
-        </p>
-
-        <p className="jost leading-[130%] tracking-[-0.02em] w-[577px] text-center text-[10px] lg:text-[25px] text-white/60">
-          Start trading with powerful tools, real-time insights, and a secure
-          platform for better profits
-        </p>
-
-        <ExternalLinkButton
-          text="Get Started Now"
-          href="/contact"
-          className="mx-auto px-8 text-black"
-        />
+    <div
+      className={clsx(
+        "min-h-300 w-screen lg:min-h-[800px] bottom-0 bg-black relative overflow-hidden",
+        className
+      )}
+    >
+      <div className="absolute flex max-h-[60%] pt-2 left-0 w-full justify-center bottom-0">
+        <div className="pointer-events-none absolute left-1/2 top-1/2 h-[400px] w-[1200px] -translate-x-1/2 -translate-y-1/2 rounded-full opacity-90 blur-[99999999999999px] bg-[radial-gradient(circle,rgba(109,59,255,0.95)_0%,rgba(91,46,255,0.75)_25%,rgba(70,30,200,0.45)_50%,rgba(0,0,0,0)_75%)]" />
+        <div className="relative bottom-0">
+          <Ellipse className="" />
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 space-y-5">
+            <Image
+              className="mx-auto"
+              src="/Frame 2121453894.png"
+              width={50}
+              height={50}
+              alt=""
+            />
+            <p className="text-transparent unbounded bg-clip-text bg-gradient-to-b from-white to-blue-800 text-[15px] lg:text-[50px] text-center capitalize font-extrabold leading-[100%] tracking-[-0.06em]">
+              Lets start your <br /> Trading Journey
+            </p>
+            <p className="jost leading-[130%] tracking-[-0.02em] w-[577px] text-center text-[10px] lg:text-[25px] text-white/60">
+              Start trading with powerful tools, real-time insights, and a secure
+              platform for better profits
+            </p>
+            <ExternalLinkButton
+              text="Get Started Now"
+              href="/contact"
+              className="mx-auto px-8 text-black"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );

24b05b0e26c4da829a25825043352072b00e48c6
BigDahn <harkinborholarholuwah@gmail.com>
2026-05-07 15:29:48 +0100

    footer arc responsiveness fixed

--- app/components/ExternalLinkButton.tsx
+++ app/components/ExternalLinkButton.tsx
@@ -1,25 +1,40 @@
-import { SquareArrowOutUpRight } from "lucide-react"
-import Link from "next/link"
-import { clsx } from "clsx"
+import { SquareArrowOutUpRight } from "lucide-react";
+import Link from "next/link";
+import { clsx } from "clsx";
 
 interface ExternalBtnLink {
   text: string;
   href: string;
   className?: string;
-  textClassName?: string
+  textClassName?: string;
 }
 
-const ExternalLinkButton = ({ href, text, className, textClassName }: ExternalBtnLink) => {
+const ExternalLinkButton = ({
+  href,
+  text,
+  className,
+  textClassName,
+}: ExternalBtnLink) => {
   return (
-    <button className={clsx("max-w-97.5 h-12 md:h-14 flex items-center justify-center bg-white gap-5 rounded-[63.18px]", className)}>
+    <button
+      className={clsx(
+        "max-w-97.5 h-9 md:h-12 lg:h-14 flex items-center justify-center bg-white gap-5 rounded-[63.18px]",
+        className,
+      )}
+    >
       <Link href={href} className="flex items-center justify-center gap-5">
-        <p className={clsx("jost font-semibold text-[14px] md:text-[20px] leading-[130%] tracking-[-2%]", textClassName)}>
+        <p
+          className={clsx(
+            "jost font-semibold text-[12px] md:text-[14px] lg:text-[20px] leading-[130%] tracking-[-2%]",
+            textClassName,
+          )}
+        >
           {text}
         </p>
         <SquareArrowOutUpRight size={20} />
       </Link>
     </button>
-  )
-}
+  );
+};
 
-export default ExternalLinkButton
+export default ExternalLinkButton;
--- app/components/FooterArc.tsx
+++ app/components/FooterArc.tsx
@@ -17,11 +17,11 @@ const FooterArc = ({ className }: FooterArcProps) => {
         className,
       )}
     >
-      <div className="absolute flex max-h-[60%] pt-2 left-0 w-full justify-center bottom-0">
-        <div className="pointer-events-none absolute left-1/2 top-1/2 h-[400px]  w-[1200px] -translate-x-1/2 -translate-y-1/2 rounded-full opacity-90 blur-[99999999999999px] bg-[radial-gradient(circle,rgba(109,59,255,0.95)_0%,rgba(91,46,255,0.75)_25%,rgba(70,30,200,0.45)_50%,rgba(0,0,0,0)_75%)]" />
-        <div className="relative bottom-0">
-          <Ellipse className=" w-[500px] md:w-screen" />
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 space-y-5">
+      <div className="absolute flex max-h-[60%] pt-2 left-0 w-full  justify-center bottom-0">
+        <div className="pointer-events-none  absolute left-1/2 top-1/2 h-[400px]  w-[1200px] -translate-x-1/2 -translate-y-1/2 rounded-full opacity-90 blur-[99999999999999px] bg-[radial-gradient(circle,rgba(109,59,255,0.95)_0%,rgba(91,46,255,0.75)_25%,rgba(70,30,200,0.45)_50%,rgba(0,0,0,0)_75%)]" />
+        <div className="relative bottom-[-9.3rem] sm:bottom-[-3.4rem] md:bottom-[-10.3rem] lg:bottom-0 ">
+          <Ellipse className="w-full h-full " />
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2 z-50 space-y-5">
             <Image
               className="mx-auto"
               src="/Frame 2121453894.png"
@@ -29,19 +29,19 @@ const FooterArc = ({ className }: FooterArcProps) => {
               height={50}
               alt=""
             />
-            <p className="text-transparent unbounded bg-clip-text bg-gradient-to-b from-white to-blue-800 text-[15px] lg:text-[50px] text-center capitalize font-extrabold leading-[100%] tracking-[-0.06em]">
+            <p className="text-transparent unbounded bg-clip-text bg-gradient-to-b from-white to-blue-800 text-[12px] md:text-[15px] lg:text-[50px] text-center capitalize font-extrabold leading-[100%] tracking-[-0.06em]">
               Lets start your <br /> Trading Journey
             </p>
-            <p className="jost leading-[130%] tracking-[-0.02em] w-[304px] md:w-[577px] text-center text-[10px] lg:text-[25px] text-white/60 ">
+            <p className="jost leading-[130%] tracking-[-0.02em] w-[220px] md:w-[508px] text-center text-[12px] md:text-[18px] lg:text-[25px] text-white/60 ">
               Start trading with powerful tools, real-time insights, and a
               secure platform for better profits
             </p>
             <ExternalLinkButton
               text="Get Started Now"
               href="/contact"
-              className="mx-auto px-8 text-black"
+              className="mx-auto px-3 md:px-8 text-black py-1 md:py-2"
             />
-            <div className="relative bottom-[254px] md:left-[725px]">
+            <div className="relative bottom-[254px] left-[230px] sm:left-[260px] md:bottom-[310px] md:left-[500px] lg:left-[725px]">
               <div className="bg-[#3E1FE8] flex justify-center h-[50px] w-[50px] items-center  rounded-full">
                 <ArrowUp color="white" />
               </div>
